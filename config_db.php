ca998ee5712a96be6d29a9afb0e05ab5a0f7a540
phantom2024 <phantom2024@hotmail.com>
2016-02-10 14:22:34 -0300

    Cambio de usuario y pass del archivo de configuracion

--- config_db.php
+++ config_db.php
@@ -1,8 +1,8 @@
 <?php
 
 define(VAR_DB,"casablancanew");
-define(VAR_USERDB,"casablancanew");
-define(VAR_PASSDB,"HNEN2sMGjTZ3Vvu3");
+define(VAR_USERDB,"root");
+define(VAR_PASSDB,"");
 define(VAR_HOST,"localhost");
 
 define(ID_DESCUENTO, 297);
